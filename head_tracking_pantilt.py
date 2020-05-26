9adb89056e58c71b7a0a56a4dba8c80eb358658e
Carlos Bravo <cbravoa@gmail.com>
2020-05-26 17:59:57 +0200

    fix boundaries

--- head_tracking_pantilt.py
+++ head_tracking_pantilt.py
@@ -116,12 +116,12 @@ def center_camera(objxy, screencenter):
         tilt_direction = -1
 
     newPan = currentPan + pan_direction * stepx  # Add or substract stepx to pan
-    newPan = newPan % pan_direction * max_angle  # To avoid having a value higher than max_angle
+    newPan = newPan % (pan_direction * max_angle)  # To avoid having a value higher than max_angle
 
     newTilt = currentTilt + tilt_direction * stepy  # Add or substract stepy to tilt
-    newTilt = newTilt % tilt_direction * max_angle  # To avoid having a value higher than max_angle
+    newTilt = newTilt % (tilt_direction * max_angle)  # To avoid having a value higher than max_angle
 
-    print(f"({objxy}) status: pan:{currentPan}, tilt:{currentTilt}; (dX:{dX}, dy:{dY}, step:{stepx}); {newPan},{newTilt}")
+    #print(f"({objxy}) status: pan:{currentPan}, tilt:{currentTilt}; (dX:{dX}, dy:{dY}, step:{stepx}); {newPan},{newTilt}")
 
     pantilthat.pan(newPan)
     pantilthat.tilt(newTilt)
